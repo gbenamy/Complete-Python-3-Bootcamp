381caf6f404896caa5633858f4239edab76c8bcb
Gilad Ben Amy <gbenamy@gbenamy-mbp.local>
2023-03-22 22:20:50 +0200

    added selection validation method

--- 04-Milestone Project - 1/Milestone1.py
+++ 04-Milestone Project - 1/Milestone1.py
@@ -1,26 +1,39 @@
+from IPython.display import clear_output
+
 def printBoard(data):
     print(data[0:3])
     print(data[3:6])
     print(data[6:9])
-
-def validateSelection(data,position):
-    if (data[position]=='   '):
-        return True
-    else:
-        return False
     
 def checkWin(data):
     #check rows
-    for
+    pass
 
     #check columns
-    for
+    pass
 
     #check diagonals
 
+def isValid(selection, data,player):
+    if selection.isdigit() == False:
+        print("Not a digit!")
+        return False
+    if int(selection) not in range (1,10):
+        print ("Not in range!")
+        return False
+    if data[int(selection)] != '   ':
+        print("Already taken!")
+        return False
+    if player == 1:
+        data[int(selection)] = ' X '
+    else:
+        data[int(selection)] = ' O '
+    
+    
+
+
+
 data =[' X ',' O ','   ',' O ',' X ',' X ',' O ',' X ',' O ']
-printBoard(data)
-validateSelection(data,2)
 
 def startGame():
     gameEnded = False
@@ -29,8 +42,11 @@ def startGame():
 
     while not gameEnded:
         valid=False
-        while not valid
-        selection=input(f'player{player} please select a position (1-9):')
+        while not valid:
+            clear_output()
+            printBoard(data)
+            selection=input(f'player{player} please select a position (1-9):')
+            valid = isValid(selection,data,player)
 
 
-
+startGame()
