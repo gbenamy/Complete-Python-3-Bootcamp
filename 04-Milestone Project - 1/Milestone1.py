bf017b524a200e747d0e802da2031fd0fb9d8b66
Gilad Ben Amy <giladba19@gmail.com.com>
2023-03-25 02:03:20 +0300

    changed data representation and printing method as part, added help print

--- 04-Milestone Project - 1/Milestone1.py
+++ 04-Milestone Project - 1/Milestone1.py
@@ -1,9 +1,23 @@
 from IPython.display import clear_output
 
-def printBoard(data):
-    print(data[0:3])
-    print(data[3:6])
-    print(data[6:9])
+def printBoard(data,demo=False):
+    temp = data[:]
+    if not demo:
+
+        for index,value in enumerate (data):
+            if data[index]==1:
+                temp[index]='X'
+            elif data[index]==2:
+                temp[index]='O'
+            else:
+                temp[index]=' '
+
+    print(" {} | {} | {} ".format(temp[0],temp[1],temp[2]))
+    print("---|---|---")
+    print(" {} | {} | {} ".format(temp[3],temp[4],temp[5]))
+    print("---|---|---")
+    print(" {} | {} | {} ".format(temp[6],temp[7],temp[8]))
+    
     
 def checkWin(data):
     #check rows
@@ -14,39 +28,63 @@ def checkWin(data):
 
     #check diagonals
 
+    return False
+
+def printHelp():
+    print("Board Positions:")
+    print("----------------")
+    printBoard([1,2,3,4,5,6,7,8,9],True)
+    print("print 'help' to see it again\n")
+
+
 def isValid(selection, data,player):
+    if selection == 'help':
+        printHelp()
+        return False
     if selection.isdigit() == False:
         print("Not a digit!")
         return False
     if int(selection) not in range (1,10):
         print ("Not in range!")
         return False
-    if data[int(selection)] != '   ':
+    if data[int(selection)] != 0:
         print("Already taken!")
         return False
-    if player == 1:
-        data[int(selection)] = ' X '
-    else:
-        data[int(selection)] = ' O '
+    data[int(selection)-1] = player
+
     
-    
+#  1 | 2 | 3 
+# ---|---|---
+#  4 | 5 | 6 
+# ---|---|---
+#  7 | 8 | 9 
 
-
-
-data =[' X ',' O ','   ',' O ',' X ',' X ',' O ',' X ',' O ']
+data =[1,0,1,0,2,0,0,2,0]
 
 def startGame():
+    data=[0,0,0,0,0,0,0,0,0]
     gameEnded = False
     player=1
     moves=0
 
     while not gameEnded:
         valid=False
+        selection = 0
         while not valid:
             clear_output()
             printBoard(data)
             selection=input(f'player{player} please select a position (1-9):')
             valid = isValid(selection,data,player)
-
-
+        data[selection]=player
+        gameEnded = checkWin()
+        if (player==1):
+            player=2
+        else:
+            player=1
+        moves+=1
+        if moves==9:
+            print("Board is full!\nNo one won")
+            gameEnded=True
+        
+printHelp()
 startGame()
