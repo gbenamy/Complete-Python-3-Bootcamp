f7d404226cbf2a039bd1388dfacb58c1f3e535d1
Gilad Ben Amy <giladba19@gmail.com.com>
2023-03-25 22:11:27 +0300

    added win check, completed game flow

--- 04-Milestone Project - 1/Milestone1.py
+++ 04-Milestone Project - 1/Milestone1.py
@@ -20,13 +20,25 @@ def printBoard(data,demo=False):
     
     
 def checkWin(data):
+#  0 | 1 | 2 
+# ---|---|---
+#  3 | 4 | 5 
+# ---|---|---
+#  6 | 7 | 8 
+
     #check rows
-    pass
+    for index in range(3):
+        if ((data[0+index*3]==data[1+index*3]==data[2+index*3]) and data[0+index*3]!=0):
+            return True
 
     #check columns
-    pass
+    for index in range(3):
+        if ((data[0+index]==data[3+index]==data[6+index]) and data[0+index]!=0):
+            return True
 
     #check diagonals
+    if (((data[0]==data[4]==data[8]) and data[0]!=0) or ((data[2]==data[4]==data[6]) and data[2]!=0)):
+        return True 
 
     return False
 
@@ -47,11 +59,11 @@ def isValid(selection, data,player):
     if int(selection) not in range (1,10):
         print ("Not in range!")
         return False
-    if data[int(selection)] != 0:
+    if data[int(selection)-1] != 0:
         print("Already taken!")
         return False
     data[int(selection)-1] = player
-
+    return True
     
 #  1 | 2 | 3 
 # ---|---|---
@@ -61,7 +73,7 @@ def isValid(selection, data,player):
 
 data =[1,0,1,0,2,0,0,2,0]
 
-def startGame():
+def startGame(debug=False):
     data=[0,0,0,0,0,0,0,0,0]
     gameEnded = False
     player=1
@@ -75,8 +87,12 @@ def startGame():
             printBoard(data)
             selection=input(f'player{player} please select a position (1-9):')
             valid = isValid(selection,data,player)
-        data[selection]=player
-        gameEnded = checkWin()
+        if (checkWin(data)):
+            gameEnded=True
+            printBoard(data)
+            print(f'Player{player} has won!')
+            break
+        
         if (player==1):
             player=2
         else:
@@ -84,6 +100,7 @@ def startGame():
         moves+=1
         if moves==9:
             print("Board is full!\nNo one won")
+            printBoard(data)
             gameEnded=True
         
 printHelp()
