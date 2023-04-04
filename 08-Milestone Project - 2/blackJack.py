ed770c0875dc1454c766cee5cfb60fc2b1bd608b
Gilad Ben Amy <giladba19@gmail.com.com>
2023-04-04 22:14:06 +0300

    added Player class, printHand function, game start initialization and started game logic

--- 08-Milestone Project - 2/blackJack.py
+++ 08-Milestone Project - 2/blackJack.py
@@ -3,8 +3,8 @@ import random
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 paint = {'Hearts':'❤️','Diamonds':'♦️', 'Spades':'♠️', 'Clubs':'♣️'}
 ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
-values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, 
-            '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8,
+          '9':9, '10':10, 'J':11, 'Q':12, 'K':13, 'A':14}
 
 
 
@@ -19,6 +19,7 @@ class Card:
 
 
 c1 = Card('Hearts', '5')
+c2 = Card('Clubs', '6')
 temp = print (c1)
 print (temp)
 
@@ -36,7 +37,7 @@ class Deck:
     
     def deal_card(self):
         return self.cards.pop()
-    
+    #TODO need to check if there are cards to deal, if not, need a new deck
 
 
 d1 = Deck()
@@ -44,3 +45,61 @@ d1.shuffle()
 
 for i in range(5):
     print(d1.deal_card())
+
+
+
+class Player:
+    def __init__(self,name,chips):
+        self.name = name
+        self.chips = chips
+    
+    def __str__(self):
+        return f'Player {self.name} has {self.chips}$'
+    
+    def increaseFunds(self,num):
+        self.chips+=num
+    
+    def decreaseFunds(self,num):
+        self.chips-=num
+
+    def getFunds(self):
+        return int(self.chips)
+    
+
+p1 = Player("p1",200)
+
+print (p1)
+print (p1.getFunds())
+p1.increaseFunds(20)
+p1.decreaseFunds(3)
+
+
+def printHand(c1,c2):
+    if c1 == '**':
+        print('|**|')
+    else:
+        print(c1)
+    print(c2)
+
+
+printHand('**',c2)
+
+
+gameOn = True
+name = input("Please enter your name:")
+chips = input("Please enter the amount of chips:")
+#TODO input validation
+p1 = Player(name,chips)
+d1 = Deck()
+d1.shuffle()
+p1_c1=d1.deal_card()
+dealer_c1=d1.deal_card()
+p1_c2=d1.deal_card()
+dealer_c2=d1.deal_card()
+
+print(dealer_c2)
+
+while (gameOn):
+    p1c1=
+
+   
